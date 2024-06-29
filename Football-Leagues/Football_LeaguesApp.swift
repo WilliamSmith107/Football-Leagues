0bd66d193290dd9dda21f9453e3be0e2130f6858
William Smith <WilliamSmith107@icloud.com>
2024-06-29 12:04:59 +0100

    Initial Commit

---
+++ Football-Leagues/Football_LeaguesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_LeaguesApp.swift
+//  Football-Leagues
+//
+//  Created by William Smith on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Football_LeaguesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
